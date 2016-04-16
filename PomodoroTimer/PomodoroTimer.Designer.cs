f4be9a0788261f21d959efb7434ba752623d0b4f
Liu <liudongyue@ap.corp.yr.com>
2016-04-16 18:44:58 +0800

    init

---
+++ PomodoroTimer/PomodoroTimer.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace PomodoroTimer
+{
+    partial class PomodoroTimer
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PomodoroTimer));
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.lbl_runtime = new System.Windows.Forms.Label();
+            this.lbl_tip = new System.Windows.Forms.Label();
+            this.lb_date_point = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ni_pomodorotimer = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(58, 34);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "开始";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Enabled = false;
+            this.btn_stop.Location = new System.Drawing.Point(58, 63);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 2;
+            this.btn_stop.Text = "停止";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // lbl_runtime
+            // 
+            this.lbl_runtime.AutoSize = true;
+            this.lbl_runtime.Location = new System.Drawing.Point(128, 9);
+            this.lbl_runtime.Name = "lbl_runtime";
+            this.lbl_runtime.Size = new System.Drawing.Size(11, 12);
+            this.lbl_runtime.TabIndex = 3;
+            this.lbl_runtime.Text = "0";
+            // 
+            // lbl_tip
+            // 
+            this.lbl_tip.AutoSize = true;
+            this.lbl_tip.Location = new System.Drawing.Point(39, 9);
+            this.lbl_tip.Name = "lbl_tip";
+            this.lbl_tip.Size = new System.Drawing.Size(59, 12);
+            this.lbl_tip.TabIndex = 4;
+            this.lbl_tip.Text = "运行时间:";
+            // 
+            // lb_date_point
+            // 
+            this.lb_date_point.FormattingEnabled = true;
+            this.lb_date_point.ItemHeight = 12;
+            this.lb_date_point.Location = new System.Drawing.Point(6, 20);
+            this.lb_date_point.Name = "lb_date_point";
+            this.lb_date_point.Size = new System.Drawing.Size(164, 352);
+            this.lb_date_point.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lb_date_point);
+            this.groupBox1.Location = new System.Drawing.Point(9, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(178, 383);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "运行记录";
+            // 
+            // ni_pomodorotimer
+            // 
+            this.ni_pomodorotimer.Icon = ((System.Drawing.Icon)(resources.GetObject("ni_pomodorotimer.Icon")));
+            this.ni_pomodorotimer.Text = "notifyIcon1";
+            this.ni_pomodorotimer.Visible = true;
+            this.ni_pomodorotimer.Click += new System.EventHandler(this.ni_pomodorotimer_Click);
+            // 
+            // PomodoroTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 480);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbl_tip);
+            this.Controls.Add(this.lbl_runtime);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "PomodoroTimer";
+            this.ShowInTaskbar = false;
+            this.Text = "番茄闹钟";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PomodoroTimer_FormClosing);
+            this.SizeChanged += new System.EventHandler(this.PomodoroTimer_SizeChanged);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Label lbl_runtime;
+        private System.Windows.Forms.Label lbl_tip;
+        private System.Windows.Forms.ListBox lb_date_point;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NotifyIcon ni_pomodorotimer;
+    }
+}
+
---
+++ PomodoroTimer/PomodoroTimer.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PomodoroTimer
+{
+    public partial class PomodoroTimer : Form
+    {
+        private int iDateMinute = 0;
+        private int iDateSecond = 0;
+
+        private System.Timers.Timer mainTimer = null;
+
+        //工作时间
+        private System.Timers.Timer workTimer = null;
+        //休息时间
+        private System.Timers.Timer breakTimer = null;
+
+        #region Test
+        ////每次工作30分钟
+        //private int iWorkInterval = 1000 * 10; 
+        ////每次休息5分钟
+        //private int iBreakInterval = 1000 * 5;
+        #endregion
+
+        //每次工作30分钟
+        private int iWorkInterval = 1000 * 60 * 30;
+        //每次休息5分钟
+        private int iBreakInterval = 1000 * 60 * 5;
+
+        private delegate void MainTimerDelegate();
+        private MainTimerDelegate mainTimerDelegate = null;
+        private MainTimerDelegate addDatePointDelegate = null;
+
+
+        public PomodoroTimer()
+        {
+            InitializeComponent();
+
+            mainTimerDelegate = new MainTimerDelegate(ShowTimerResult);
+            addDatePointDelegate = new MainTimerDelegate(AddDatePoint2ListBoxItem);
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            this.btn_start.Enabled = false;
+            this.btn_stop.Enabled = true;
+            WorkTimerStart();
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            this.btn_start.Enabled = true;
+            this.btn_stop.Enabled = false;
+            DisposeTimer();
+        }
+
+        private void WorkTimerStart()
+        {
+            DisposeTimer();
+            MainTimerStart();
+            
+            InitTimer("work",ref workTimer, iWorkInterval);
+        }
+
+        private void WorkTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            this.Invoke(addDatePointDelegate);
+
+            workTimer.Stop();
+            BreakTimerStart();
+
+            MessageBox.Show("啊啊啊啊啊啊,你还不站起来,扭扭脖子动动腿!", "保命提示!请浪5分钟!", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+
+        }
+
+        private void BreakTimerStart()
+        {
+            DisposeTimer();
+            MainTimerStart();
+             
+            InitTimer("break",ref breakTimer, iBreakInterval);
+        }
+
+        private void BreakTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            breakTimer.Stop();
+            WorkTimerStart();
+
+            MessageBox.Show("休息结束,请干活!", "保命提示!", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+        }
+
+        private void MainTimerStart()
+        {
+            InitTimer("main",ref mainTimer, 1000);
+        }
+
+        private void MainTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            this.Invoke(mainTimerDelegate);
+        }
+
+        private void ShowTimerResult()
+        {
+            iDateSecond++;
+            if (iDateSecond % 60 == 0)
+            {
+                iDateSecond = 0;
+                iDateMinute++;
+            }
+
+            this.lbl_runtime.Text = string.Format("{0}分{1}秒", iDateMinute, iDateSecond);
+        }
+        /// <summary>
+        /// 往listbox中添加时间点
+        /// </summary>
+        private void AddDatePoint2ListBoxItem()
+        {
+            this.lb_date_point.Items.Add(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+
+        /// <summary>
+        /// 节约内存
+        /// </summary>
+        private void DisposeTimer()
+        {
+            iDateMinute = 0;
+            iDateSecond = 0;
+
+            if (workTimer != null)
+            {
+                workTimer.Stop();
+                workTimer.Close();
+                workTimer.Dispose();
+            }
+            if (breakTimer != null)
+            {
+                breakTimer.Stop();
+                breakTimer.Close();
+                breakTimer.Dispose();
+            }
+            if (mainTimer != null)
+            {
+                mainTimer.Stop();
+                mainTimer.Close();
+                mainTimer.Dispose();
+            }
+        }
+
+
+        private void InitTimer(string name,ref System.Timers.Timer timer,int iInterval)
+        {
+            timer = new System.Timers.Timer();
+            timer.Interval = iInterval;
+            timer.Enabled = true;
+            switch(name)
+            {
+                case "main":
+                    timer.Elapsed += MainTimer_Elapsed;
+                    break;
+                case "work":
+                    timer.Elapsed += WorkTimer_Elapsed;
+                    break;
+                case "break":
+                    timer.Elapsed += BreakTimer_Elapsed;
+                    break;
+            }
+
+        }
+
+        private void PomodoroTimer_SizeChanged(object sender, EventArgs e)
+        {
+            if(WindowState==FormWindowState.Minimized)
+            {
+                Hide();
+                ni_pomodorotimer.Visible = true;
+            }
+        }
+
+        private void ni_pomodorotimer_Click(object sender, EventArgs e)
+        {
+            Visible = true;
+            WindowState = FormWindowState.Normal;
+            ni_pomodorotimer.Visible = false;
+        }
+
+        private void PomodoroTimer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("确定关闭吗?", "关闭", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if(dr==DialogResult.OK)
+            {
+                DisposeTimer();
+                Application.ExitThread();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
